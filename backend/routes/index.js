2eb13594b3451153f354e1b648e4231020822069
goku <coding.game@gmail.com>
2019-05-07 21:58:47 -0400

    Fix misc.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { catchErrors } = require('../handlers/errorHandlers')
-const { postLogin, getImprint } = require('./login')
+const { postLogin } = require('./login')
 
 // console.log(postLogin, getImprint, require('./login'), 'ssss')
 router.post('/login', catchErrors(postLogin))
