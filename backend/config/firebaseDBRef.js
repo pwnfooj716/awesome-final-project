c0aee8ebe60cfa402dfdd0f7d8cd9aa7ad7cd41c
goku <coding.game@gmail.com>
2019-05-09 16:17:20 -0400

    Clean code.

--- backend/config/firebaseDBRef.js
+++ backend/config/firebaseDBRef.js
@@ -2,14 +2,12 @@ var admin = require("firebase-admin");
 
 const getCollection = (collectionName) => {
   return async () => {
-    let targetCollection;
     const db = admin.firestore();
-    // targetCollection = db.collection(collectionName);
-    // console.log(targetCollection, targetCollection.get, '123')
     return db.collection(collectionName);
   }
 }
 
 module.exports = {
-  users: getCollection('/users')
+  users: getCollection('/users'),
+  posts: getCollection('/posts')
 }
