f30422a70c79082fa7044de90c05732a1d12f12c
Yide Zhao <353825242@qq.com>
2019-05-11 21:48:38 -0400

    Create ChangeAvator.js

---
+++ frontend/src/components/ChangeAvator.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import AddAPhoto from "@material-ui/icons/AddAPhotoOutlined";
+import AddToPhoto from "@material-ui/icons/AddToPhotosRounded";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import AvatorEditor from "./AvatorEditor";
+
+class ChangeAvator extends Component {
+  state = {
+    input: this.props.image,
+    open: false
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleUpload = event => {
+    var file = event.target.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this.setState({
+        input: reader.result
+      });
+    };
+    reader.onerror = function(error) {
+      console.log("Error: ", error);
+    };
+  };
+
+  handleEditEvent = event => {
+    this.setState({ input: event });
+    this.handleClose();
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    let preview = "";
+    if (this.state.input) {
+      preview = (
+        <DialogContent>
+          <DialogContentText>
+            <AvatorEditor
+              image={this.state.input}
+              handleEditEvent={this.handleEditEvent}
+            />
+          </DialogContentText>
+        </DialogContent>
+      );
+    }
+    return (
+      <div>
+        <ListItem>
+          <IconButton
+            aria-label="Add to favorites"
+            onClick={this.handleClickOpen}
+          >
+            <AddAPhoto />
+          </IconButton>
+          <ListItemText primary="Upload New Avator Image" />
+        </ListItem>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="draggable-dialog-title"
+        >
+          <DialogTitle id="draggable-dialog-title">
+            <IconButton
+              aria-label="Add to favorites"
+              onClick={this.handleClickOpen}
+            >
+              <AddToPhoto color="secondary" />
+            </IconButton>
+            Upload Image
+          </DialogTitle>
+          {preview}
+          <DialogActions>
+            <Button color="primary">
+              <input type="file" onChange={this.handleUpload.bind(this)} />
+            </Button>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default ChangeAvator;
