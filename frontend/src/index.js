0cddb6e142315cc91d18602f8db2bbd3fb405311
Yide Zhao <353825242@qq.com>
2019-05-14 14:43:19 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "../node_modules/react-cropper-image-editor/node_modules/cropperjs/dist/cropper.css";
 import { Provider } from "react-redux";
 import configureStore from "./store";
 
